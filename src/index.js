060f484fa5fdc0f142cefa995946bb76ff08ccca
Ashraj Grewal <ashraj.grewal10@gmail.com>
2023-08-27 23:17:02 -0600

    completed string-lib challenge 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+//Challenge 1 capitalize() -makes the first character of a given string uppercase.
 const str = 'hello world'
 
-const first = str[0].toUpperCase()
-const rest = str.slice(1)
+function capitalize(str) {
+    const first = str[0].toUpperCase()
+    const rest = str.slice(1)
+    return first + rest
+}
 
-console.log(first + rest)
+console.log(capitalize(str))
+console.log(capitalize('str'))
+
+//Challenge 2 allCaps()- makes all characters uppercase. (this is the same as str.toUpperCase())
+
+function allCaps(str) {
+    after = str.toUpperCase()
+    return after
+}
+
+console.log(allCaps(str))
+console.log(allCaps('str'))
